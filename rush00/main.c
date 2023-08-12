a811cada9810cedfcf3f971011f3a2198d6cb139
Rocky Kamen-Rubio <rokamen-@cbr15s5.42barcelona.com>
2023-08-12 15:28:53 +0200

    revises compiling everything at once

--- rush00/main.c
+++ rush00/main.c
@@ -6,66 +6,16 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 13:40:17 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/12 13:57:57 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/12 15:02:13 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
-void	ft_put_top_or_bottom(int x, int y)
-{
-	int	i;
+void	rush(int x, int y);
 
-	ft_putchar('A');
-	i = 0;
-	while (i < x - 2)
-	{
-		ft_putchar('B');
-		i++;
-	}
-	if (x > 1)
-		ft_putchar('C');
-	ft_putchar('\n');
-}
-
-void	ft_put_middle(int x, int y)
-{
-	int	i;
-
-	if (x == 0 || y == 0)
-		return ;
-	ft_putchar('B');
-	i = 0;
-	while (i < x - 2)
-	{
-		ft_putchar(' ');
-		i++;
-	}
-	if (x > 1)
-		ft_putchar('B');
-	ft_putchar('\n');
-}
-
-void	rush(int x, int y)
-{
-	int	i;
-
-	ft_put_top_or_bottom(x, y);
-	i = 0;
-	while (i < y - 2)
-	{
-		ft_put_middle(x, y);
-		i++;
-	}
-	if (y > 1)
-		ft_put_top_or_bottom(x, y);
-}
-/*
 void	eval_vals(int x, int y)
 {
 	char	char_x;
@@ -81,17 +31,14 @@ void	eval_vals(int x, int y)
 	rush(x, y);
 	write(1, "\n", 1);
 }
-*/
-/*
-eval_vals(5, 3);
-eval_vals(5, 1);
-eval_vals(1, 1);
-eval_vals(1, 5);
-eval_vals(4, 4);
-*/
 
 int	main(void)
 {
+	eval_vals(5, 3);
+	eval_vals(5, 1);
+	eval_vals(1, 1);
+	eval_vals(1, 5);
+	eval_vals(4, 4);
 	rush(5, 3);
 	ft_putchar('\n');
 	rush(5, 1);

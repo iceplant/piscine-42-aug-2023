4571b687d9bb7b11206e45935f780dea655a02dc
Rocky Kamen-Rubio <rokamen-@cbr15s5.42barcelona.com>
2023-08-12 12:21:28 +0200

    C00-08 passes norm, should be working now

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -6,29 +6,39 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 19:14:10 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/12 12:11:49 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/12 12:19:43 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_strlen(char *str)
-{
-	int	n;
-
-	n = 0;
-	while (str[n] != '\0')
-	{
-		n++;
-	}
-	return (n);
-}
-
-// n = length of array
+// n = length of array to display
 void	ft_putstr(char *str, int n)
 {
 	write(1, str, n);
 	write(1, ", ", 2);
+}
+
+void	ft_initialize_vals(char *vals, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		vals[i] = i + '0';
+		i = i + 1;
+	}
+}
+
+void	ft_last_digit(char *vals, int n)
+{
+	ft_putstr(vals, n);
+	while (vals[n - 1] < '9')
+	{
+		vals[n - 1] = vals[n - 1] + 1;
+		ft_putstr(vals, n);
+	}
 }
 
 void	ft_print_combn(int n)
@@ -39,21 +49,11 @@ void	ft_print_combn(int n)
 	int		count;
 
 	count = 0;
-	i = 0;
-	while (i < n)
-	{
-		vals[i] = i + '0';
-		i = i + 1;
-	}
 	i = n - 1;
+	ft_initialize_vals(vals, n);
 	while (vals[0] != 9 - n + 1 + '0')
 	{
-		ft_putstr(vals, n);
-		while (vals[n - 1] < '9')
-		{
-			vals[n - 1] = vals[n - 1] + 1;
-			ft_putstr(vals, n);
-		}
+		ft_last_digit(vals, n);
 		while (vals[i] == 9 - n + i + 1 + '0')
 		{
 			i--;

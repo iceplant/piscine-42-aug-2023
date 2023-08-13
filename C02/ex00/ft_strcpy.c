f15cd05948ed6e9905549c5e96de2196a615d621
Rocky Kamen-Rubio <rokamen-@cbr1s4.42barcelona.com>
2023-08-13 16:44:58 +0200

    norms ex00

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -6,22 +6,26 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 14:09:40 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/13 16:32:07 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/13 16:44:47 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	iter;
 
-char *ft_strcpy(char *dest, char *src) {
-	int iter;
 	iter = 0;
-	
-	while (src[iter] != '\0') {
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	while (src[iter] != '\0')
+	{
 		*(dest + iter) = *(src + iter);
 		iter++;
 	}
 	*(dest + iter) = '\0';
-	return dest;
+	return (dest);
 }
-

f105c2e58ddfd712f5b6b2c83bf67099471586c4
Rocky Kamen-Rubio <rokamen-@cbr1s2.42barcelona.com>
2023-08-13 19:59:25 +0200

    adds working? ex07

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/13 19:44:33 by rokamen-          #+#    #+#             */
+/*   Updated: 2023/08/13 19:55:51 by rokamen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_char_is_lower(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
+char	*ft_strupcase(char *str)
+{
+	int	iter;
+
+	iter = 0;
+	while (*(str + iter) != '\0')
+	{
+		if (ft_char_is_lower(*(str + iter)))
+			*(str + iter) = *(str + iter) - 32;
+		iter++;
+	}
+	return (str);
+}
---
+++ C02/ex07/test_ft_strupcase.c
@@ -0,0 +1,7 @@
+#include "ft_strupcase.c"
+#include <stdio.h>
+
+int main(void) {
+	char str[] = "abcde123";
+	printf("Make abcde123 upper case: %s\n", ft_strupcase(str));
+}

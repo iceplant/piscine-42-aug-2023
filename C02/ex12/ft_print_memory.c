4c4b315a6c5f507c31a9684937fe754819d43ae9
Rocky Kamen-Rubio <rokamen-@cbr3s4.42barcelona.com>
2023-08-15 14:30:12 +0200

    some work on ex12

---
+++ C02/ex12/ft_print_memory.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_memory.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 13:05:38 by rokamen-          #+#    #+#             */
+/*   Updated: 2023/08/15 14:25:55 by rokamen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_write_char_hex_code_helper(char c)
+{
+	int		div;
+	int		mod;
+	char	last_digit_char;
+
+	if (c == 0)
+		return ;
+	div = c / 16;
+	mod = c % 16;
+	if (mod > 9)
+		last_digit_char = mod + 'a' - 10;
+	else
+		last_digit_char = mod + '0';
+	ft_write_char_hex_code_helper(div);
+	write(1, &last_digit_char, 1);
+}
+
+void	ft_write_char_hex_code(char c)
+{
+	if (c == 0)
+		write(1, "0", 1);
+	else
+	{
+		write(1, "\\", 1);
+		ft_write_char_hex_code_helper(c);
+	}
+}
+
+void *ft_print_memory(void *addr, unsigned int size) {
+	if (size == 0) return 0;
+
+	int iter;
+	iter = 0;
+	char *char_addr = (char *) addr;
+	while (iter < size) {
+		printf("iter: %d ", iter);
+		printf(" %p: ", char_addr + iter);
+		int iter2 = iter;
+		while (iter2 < iter + 16 ) {
+			if (iter2 < size) {
+				printf("%x%x ", char_addr[iter2], char_addr[iter2+1]);
+			} else {
+				printf("     ");
+			}
+			iter2 += 2;
+		}
+		int iter3 = iter;
+		while (iter3 < iter + 16) {
+			printf("%c", char_addr[iter3]);
+			iter3++;
+		}
+		printf("\n");
+		iter += 16;
+	}
+
+	return 0;
+}
+
---
+++ C02/ex12/test_ft_print_memory.c
@@ -0,0 +1,9 @@
+#include "ft_print_memory.c"
+#include <stdio.h>
+
+void *str = "Bonjour les aminches...c. est fou.tou.ce que on peut faire avec...print_memory....lol.lol. .";
+
+int main(void){
+	//void *str = "Hello world!";
+	ft_print_memory(str, 100);
+}

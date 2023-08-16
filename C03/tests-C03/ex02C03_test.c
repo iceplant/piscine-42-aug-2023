becfb28f3cbb9669be8985fc0519d020a6b6a09a
Rocky Kamen-Rubio <rokamen-@cbr9s6.42barcelona.com>
2023-08-16 13:30:29 +0200

    updates testC0302

--- C03/tests-C03/ex02C03_test.c
+++ C03/tests-C03/ex02C03_test.c
@@ -1,13 +1,14 @@
 #include "../ex02/ft_strcat.c"
 #include <stdio.h>
-
+#include <assert.h>
 #include <string.h>
 
 int main(void) {
 	char str1[] = "abcde";
 	char str2[] = "qwert";
 	ft_strcat(str2, str1);
-	printf("%s", str2);
+	assert(strcmp(str2, "qwertabcde") == 0);
+	//printf("%s", str2);
 
 
 }

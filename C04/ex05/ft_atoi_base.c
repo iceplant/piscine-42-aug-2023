37179f28c6cef4f7a609c1f6fc6b66b147ef9fab
Rocky Kamen-Rubio <rokamen-@cbr9s6.42barcelona.com>
2023-08-16 22:55:04 +0200

    almost linted ex05

--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -6,7 +6,7 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 21:15:29 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/16 22:12:49 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/16 22:54:28 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,83 +19,90 @@ int	ft_isspace(char c)
 }
 
 // returns -1 if value is not found in base
-int ft_char_to_val(char c, char *base) {
-	int i = 0;
-	while (base[i] != '\0') {
-		if (base[i] == c) {
-			return i;
+int	ft_char_to_val(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == c)
+		{
+			return (i);
 		}
 		i++;
 	}
-	return -1;
+	return (-1);
 }
 
-	
+// process base: get its length, check for forbidden chars (+- ),
+//	check for repeated chars
 
-//process base: get its length, check for forbidden chars (+- ), check for repeated chars
+int	ft_check_base(char *base)
+{
+	int	base_len;
+	int	ibase;
 
-int ft_check_base(char *base) {
-	int base_len;
-	int ibase;
 	base_len = 0;
 	while (*(base + base_len) != '\0')
 	{
-		//printf("base_len: %d\n", base_len);
 		ibase = 0;
-		if ((base[base_len] == '+' || base[base_len] == '-') || base[base_len] == ' ')
+		if ((base[base_len] == '+' || base[base_len] == '-')
+			|| base[base_len] == ' ')
 			return (-1);
 		while (ibase < base_len - 1)
 		{
-		//	printf("inner loop\n");
-			if (base[base_len] == base[ibase]) {
+			if (base[base_len] == base[ibase])
+			{
 				printf("reached inner inner block");
 				return (-1);
 			}
 			ibase++;
 		}
-		//printf("reached past inner loop");
 		base_len++;
 	}
-	//printf("reached past base loop");
 	return (base_len);
 }
 
+int	handle_sign(char *str, int istr)
+{
+	int	sign;
 
-int ft_atoi_base(char *str, char *base) {
+	sign = 1;
+	while (str[istr] == '-' || str[istr] == '+')
+	{
+		if (str[istr] == '-')
+		{
+			sign *= -1;
+		}
+		istr++;
+	}
+	return (sign); //this returns sign but we need to return info about the istr that has been incremented too 
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
 	int	istr;
 	int	sign;
 	int	so_far;
+	int	base_len;
 
 	istr = 0;
-	//check str for whitespace
 	while (ft_isspace(str[istr]))
 	{
 		istr++;
 	}
-	sign = 1;
-	// account for leading signs on str
-	while (str[istr] == '-' || str[istr] == '+')
-	{
-		if (str[istr] == '-')
-			sign *= -1;
-		istr++;
-	}
-
-	int base_len;
+	sign = handle_sign(str, istr);
 	base_len = ft_check_base(base);
-	if (base_len == -1) return (0);
-	
-	
+	if (base_len == -1)
+		return (0);
 	so_far = 0;
-	// convert to int
 	while (ft_char_to_val(str[istr], base) != -1 && str[istr] != '\0')
 	{
-		printf("%c", str[istr]);
 		so_far *= base_len;
 		so_far += ft_char_to_val(str[istr], base);
 		istr++;
 	}
-	
 	so_far *= sign;
 	return (so_far);
 }
--- C04/tests-C04/ex05C04_test.c
+++ C04/tests-C04/ex05C04_test.c
@@ -4,5 +4,8 @@
 int main(void) {
 	printf("%d\n", ft_check_base("0123456789"));
 	printf("char %c with base %s converts to %d\n", 'f', "0123456789abcdef", ft_char_to_val('f', "0123456789abcdef"));
-	ft_atoi_base("   +++-+-+-12345", "0123456789");
+	int d = ft_atoi_base("   +++-+-+-12345", "0123456789");
+	printf("string atois to %d\n", d);
+	int d2 = ft_atoi_base("   ---+-+-1d2e3aj45", "0123456789abcdef");
+	printf("string atois to %d\n", d2);
 }

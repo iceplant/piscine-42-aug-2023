92459c4051e78157e46758614aa91b50bfa5ddce
Rocky Kamen-Rubio <rokamen-@cbr5s4.42barcelona.com>
2023-08-20 15:49:00 +0200

    more lintinng

---
+++ rush01/ex01/edges.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   edges.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/20 15:39:20 by rokamen-          #+#    #+#             */
+/*   Updated: 2023/08/20 15:41:11 by rokamen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	get_possible_rows_from_edges_helper(char **arr, char e1, char e2)
+{
+	if (e1 == '1' && e2 == '4')
+		arr[0] = "4321";
+	if (e1 == '4' && e2 == '1')
+		arr[0] = "1234";
+	if (e1 == '2' && e2 == '2')
+	{
+		arr[0] = "2143";
+		arr[1] = "2413";
+		arr[2] = "3142";
+		arr[3] = "3412";
+	}
+	if (e1 == '2' && e2 == '3')
+	{
+		arr[0] = "1432";
+		arr[1] = "2431";
+		arr[2] = "3421";
+	}
+	if (e1 == '3' && e2 == '2')
+	{
+		arr[0] = "2341";
+		arr[1] = "1342";
+		arr[2] = "1243";
+	}
+}
+
+void	get_possible_rows_from_edges_helper2(char **arr, char e1, char e2)
+{
+	if (e1 == '1' && e2 == '2')
+	{
+		arr[0] = "4123";
+		arr[1] = "4213";
+	}
+	if (e1 == '2' && e2 == '1')
+	{
+		arr[0] = "3214";
+		arr[1] = "3124";
+	}
+	if (e1 == '1' && e2 == '3')
+	{
+		arr[0] = "4231";
+		arr[1] = "4132";
+		arr[2] = "4312";
+	}
+	if (e1 == '3' && e2 == '1')
+	{
+		arr[0] = "1324";
+		arr[1] = "2314";
+		arr[2] = "2134";
+	}
+}
+
+// make this shorter? implement some kind of db?
+// idea: for e1 > e2 call the function backwards and reverse
+char	**get_possible_rows_from_edges(char e1, char e2)
+{
+	char	**arr;
+
+	arr = (char **)malloc(4 * sizeof(char *));
+	get_possible_rows_from_edges_helper(arr, e1, e2);
+	get_possible_rows_from_edges_helper2(arr, e1, e2);
+	return (arr);
+}
--- rush01/ex01/solve.c
+++ rush01/ex01/solve.c
@@ -6,104 +6,39 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 13:39:40 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/20 14:55:04 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/20 15:40:46 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 //#include "board.c"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int	ft_strstrlen(char **str);
-
+int		ft_strstrlen(char **str);
 
 int		check_if_heights_1_through_n(char *heights, int size);
 void	ft_print_board(char board[4][4]);
 char	*ft_get_row_from_board(char board[4][4], int row);
 char	*ft_get_column_from_board(char board[4][4], int col);
 
+void	get_possible_rows_from_edges_helper(char **arr, char e1, char e2);
 
-
-void	get_possible_rows_from_edges_helper(char **arr, char e1, char e2)
-{
-	if (e1 == '1' && e2 == '4')
-		arr[0] = "4321";
-	if (e1 == '4' && e2 == '1')
-		arr[0] = "1234";
-	if (e1 == '2' && e2 == '2')
-	{
-		arr[0] = "2143";
-		arr[1] = "2413";
-		arr[2] = "3142";
-		arr[3] = "3412";
-	}
-	if (e1 == '2' && e2 == '3')
-	{
-		arr[0] = "1432";
-		arr[1] = "2431";
-		arr[2] = "3421";
-	}
-	if (e1 == '3' && e2 == '2')
-	{
-		arr[0] = "2341";
-		arr[1] = "1342";
-		arr[2] = "1243";
-	}
-}
-
-void	get_possible_rows_from_edges_helper2(char **arr, char e1, char e2)
-{
-	if (e1 == '1' && e2 == '2')
-	{
-		arr[0] = "4123";
-		arr[1] = "4213";
-	}
-	if (e1 == '2' && e2 == '1')
-	{
-		arr[0] = "3214";
-		arr[1] = "3124";
-	}
-	if (e1 == '1' && e2 == '3')
-	{
-		arr[0] = "4231";
-		arr[1] = "4132";
-		arr[2] = "4312";
-	}
-	if (e1 == '3' && e2 == '1')
-	{
-		arr[0] = "1324";
-		arr[1] = "2314";
-		arr[2] = "2134";
-	}
-}
+void	get_possible_rows_from_edges_helper2(char **arr, char e1, char e2);
 
 // make this shorter? implement some kind of db?
 // idea: for e1 > e2 call the function backwards and reverse
-char	**get_possible_rows_from_edges(char e1, char e2)
-{
-	char	**arr;
+char	**get_possible_rows_from_edges(char e1, char e2);
 
-	arr = (char **) malloc(4 * sizeof(char *));
-	get_possible_rows_from_edges_helper(arr, e1, e2);
-	get_possible_rows_from_edges_helper2(arr, e1, e2);
-	return (arr);
-}
-
-int	ft_check_if_vals_satisfy_edges(char *heights, char edge1, char edge2)
+int	ft_check_if_vals_satisfy_edges_left_to_right(char *heights, char edge1,
+		char edge2)
 {
 	char	max_height_so_far1;
-	char	max_height_so_far2;
 	int		visible_count1;
-	int		visible_count2;
 	int		i1;
-	int		i2;
 
 	max_height_so_far1 = '0';
-	max_height_so_far2 = '0';
 	visible_count1 = 0;
-	visible_count2 = 0;
 	i1 = 0;
-	i2 = 3;
 	while (i1 < 4)
 	{
 		if (heights[i1] > max_height_so_far1)
@@ -113,6 +48,19 @@ int	ft_check_if_vals_satisfy_edges(char *heights, char edge1, char edge2)
 		}
 		i1++;
 	}
+	return (visible_count1 == edge1 - '0');
+}
+
+int	ft_check_if_vals_satisfy_edges_right_to_left(char *heights, char edge1,
+		char edge2)
+{
+	char	max_height_so_far2;
+	int		visible_count2;
+	int		i2;
+
+	max_height_so_far2 = '0';
+	visible_count2 = 0;
+	i2 = 3;
 	while (i2 >= 0)
 	{
 		if (heights[i2] > max_height_so_far2)
@@ -122,8 +70,14 @@ int	ft_check_if_vals_satisfy_edges(char *heights, char edge1, char edge2)
 		}
 		i2--;
 	}
-	return ((check_if_heights_1_through_n(heights, 4) && visible_count1 == edge1
-			- '0') && (visible_count2 == edge2 - '0'));
+	return (visible_count2 == edge2 - '0');
+}
+
+int	ft_check_if_vals_satisfy_edges(char *heights, char edge1, char edge2)
+{
+	return (check_if_heights_1_through_n(heights, 4)
+		&& ft_check_if_vals_satisfy_edges_left_to_right(heights, edge1, edge2)
+		&& ft_check_if_vals_satisfy_edges_right_to_left(heights, edge1, edge2));
 }
 
 int	ft_is_board_correct(char board[4][4], char *edges, int board_dimension)

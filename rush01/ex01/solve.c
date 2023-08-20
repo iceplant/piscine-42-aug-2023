32fa6bfd6503daf5c4f9fa5a8b9609d0cffa8c23
Rocky Kamen-Rubio <rokamen-@cbr5s4.42barcelona.com>
2023-08-20 19:39:34 +0200

    seems to resolve norm and compile issues

--- rush01/ex01/solve.c
+++ rush01/ex01/solve.c
@@ -6,11 +6,11 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 13:39:40 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/20 19:31:32 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/20 19:39:17 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+//#include <stdio.h>
 
 int		ft_strstrlen(char **str);
 int		check_if_heights_1_through_n(char *heights, int size);
@@ -29,7 +29,7 @@ int		ft_check_if_vals_satisfy_edges(char *heights, char edge1, char edge2);
 
 char	**get_possible_rows_from_edges(char e1, char e2);
 
-
+/**
 void	ft_print_debugger(char board[4][4], char *edges, char **rows,
 		int rows_filled)
 {
@@ -44,7 +44,7 @@ void	ft_print_debugger(char board[4][4], char *edges, char **rows,
 	printf("possible row: %s\n", rows[0]);
 	printf("edge1: %c edge2: %c num_rows: %i\n", edge1, edge2,
 		num_rows_for_edges);
-} 
+} */
 int	ft_handle_rows_filled(char board[4][4], char *edges, int board_dimension)
 {
 	if (ft_is_board_correct(board, edges, board_dimension))
@@ -59,33 +59,30 @@ int	ft_handle_rows_filled(char board[4][4], char *edges, int board_dimension)
 // edge2 = edges[12 + rows_filled];
 // num_rows_for_edges = ft_strstrlen(rows);
 // returns 1 if a solution was found, 0 if not
-int	ft_solve(char board[4][4], char *edges, int rows_filled,
+// rows_filled => r to save space
+int	ft_solve(char board[4][4], char *edges, int r,
 		int board_dimension)
 {
 	int		rtn;
 	char	**rows;
 	int		i;
 	int		j;
+	int		num_rows_for_edges;
 
-	if (rows_filled == 4)
+	if (r == 4)
 		return (ft_handle_rows_filled(board, edges, board_dimension));
-	rows = get_possible_rows_from_edges(edges[8 + rows_filled], 
-			edges[12 + rows_filled]);
-
-	ft_print_debugger(board, edges, rows, rows_filled);
-	printf("rows length: %d", ft_strstrlen(rows));
+	rows = get_possible_rows_from_edges(edges[8 + r], edges[12 + r]);
 	i = 0;
-	int num_rows_for_edges = ft_strstrlen(rows);
+	num_rows_for_edges = ft_strstrlen(rows);
 	while (i < num_rows_for_edges)
 	{
 		j = 0;
 		while (j < 4)
 		{
-			printf("i: %d, j: %d, rows_len: %d\n", i, j, ft_strstrlen(rows));
-			board[rows_filled][j] = rows[i][j];
+			board[r][j] = rows[i][j];
 			j++;
 		}
-		rtn = ft_solve(board, edges, rows_filled + 1, board_dimension);
+		rtn = ft_solve(board, edges, r + 1, board_dimension);
 		if (rtn)
 			return (rtn);
 		i++;

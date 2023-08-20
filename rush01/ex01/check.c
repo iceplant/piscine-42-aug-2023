f3a0f7400158b4c78ff4172713bc89680608b4c7
Rocky Kamen-Rubio <rokamen-@cbr5s4.42barcelona.com>
2023-08-20 16:50:54 +0200

    passes linter, working on compiling

---
+++ rush01/ex01/check.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/20 16:09:59 by rokamen-          #+#    #+#             */
+/*   Updated: 2023/08/20 16:48:09 by rokamen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int		ft_strstrlen(char **str);
+int		check_if_heights_1_through_n(char *heights, int size);
+void	ft_print_board(char board[4][4]);
+char	*ft_get_row_from_board(char board[4][4], int row);
+char	*ft_get_column_from_board(char board[4][4], int col);
+void	get_possible_rows_from_edges_helper(char **arr, char e1, char e2);
+void	get_possible_rows_from_edges_helper2(char **arr, char e1, char e2);
+
+int	ft_check_if_vals_satisfy_edges_left_to_right(char *heights, char edge1)
+{
+	char	max_height_so_far1;
+	int		visible_count1;
+	int		i1;
+
+	max_height_so_far1 = '0';
+	visible_count1 = 0;
+	i1 = 0;
+	while (i1 < 4)
+	{
+		if (heights[i1] > max_height_so_far1)
+		{
+			max_height_so_far1 = heights[i1];
+			visible_count1++;
+		}
+		i1++;
+	}
+	return (visible_count1 == edge1 - '0');
+}
+
+int	ft_check_if_vals_satisfy_edges_right_to_left(char *heights,	char edge2)
+{
+	char	max_height_so_far2;
+	int		visible_count2;
+	int		i2;
+
+	max_height_so_far2 = '0';
+	visible_count2 = 0;
+	i2 = 3;
+	while (i2 >= 0)
+	{
+		if (heights[i2] > max_height_so_far2)
+		{
+			max_height_so_far2 = heights[i2];
+			visible_count2++;
+		}
+		i2--;
+	}
+	return (visible_count2 == edge2 - '0');
+}
+
+int	ft_check_if_vals_satisfy_edges(char *heights, char edge1, char edge2)
+{
+	return (check_if_heights_1_through_n(heights, 4)
+		&& ft_check_if_vals_satisfy_edges_left_to_right(heights, edge1)
+		&& ft_check_if_vals_satisfy_edges_right_to_left(heights, edge2));
+}
+
+int	ft_is_board_correct(char board[4][4], char *edges, int board_dimension)
+{
+	int	row;
+	int	column;
+
+	row = 0;
+	while (row < board_dimension)
+	{
+		if (!ft_check_if_vals_satisfy_edges(ft_get_row_from_board(board, row),
+				edges[8 + row], edges[12 + row]))
+		{
+			printf("row: %d\n", row);
+			return (0);
+		}
+		row++;
+	}
+	column = 0;
+	while (column < board_dimension)
+	{
+		if (!ft_check_if_vals_satisfy_edges(ft_get_column_from_board(board,
+					column), edges[0 + column], edges[4 + column]))
+		{
+			return (0);
+		}
+		column++;
+	}
+	return (1);
+}
--- rush01/ex01/solve.c
+++ rush01/ex01/solve.c
@@ -6,7 +6,7 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 13:39:40 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/20 15:40:46 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/20 16:45:09 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,146 +15,78 @@
 #include <stdlib.h>
 
 int		ft_strstrlen(char **str);
-
 int		check_if_heights_1_through_n(char *heights, int size);
 void	ft_print_board(char board[4][4]);
 char	*ft_get_row_from_board(char board[4][4], int row);
 char	*ft_get_column_from_board(char board[4][4], int col);
-
 void	get_possible_rows_from_edges_helper(char **arr, char e1, char e2);
-
 void	get_possible_rows_from_edges_helper2(char **arr, char e1, char e2);
 
-// make this shorter? implement some kind of db?
-// idea: for e1 > e2 call the function backwards and reverse
+int		ft_is_board_correct(char board[4][4], char *edges, int board_dimension);
+int		ft_check_if_vals_satisfy_edges_left_to_right(char *heights, char edge1,
+			char edge2);
+int		ft_check_if_vals_satisfy_edges_right_to_left(char *heights, char edge1,
+			char edge2);
+int		ft_check_if_vals_satisfy_edges(char *heights, char edge1, char edge2);
+
 char	**get_possible_rows_from_edges(char e1, char e2);
 
-int	ft_check_if_vals_satisfy_edges_left_to_right(char *heights, char edge1,
-		char edge2)
+void	ft_print_debugger(char board[4][4], char *edges, char **rows,
+		int rows_filled)
 {
-	char	max_height_so_far1;
-	int		visible_count1;
-	int		i1;
+	int	edge1;
+	int	edge2;
+	int	num_rows_for_edges;
 
-	max_height_so_far1 = '0';
-	visible_count1 = 0;
-	i1 = 0;
-	while (i1 < 4)
-	{
-		if (heights[i1] > max_height_so_far1)
-		{
-			max_height_so_far1 = heights[i1];
-			visible_count1++;
-		}
-		i1++;
-	}
-	return (visible_count1 == edge1 - '0');
+	num_rows_for_edges = ft_strstrlen(rows);
+	edge1 = edges[8 + rows_filled];
+	edge2 = edges[12 + rows_filled];
+	ft_print_board(board);
+	printf("possible row: %s\n", rows[0]);
+	printf("edge1: %c edge2: %c num_rows: %i\n", edge1, edge2,
+		num_rows_for_edges);
 }
 
-int	ft_check_if_vals_satisfy_edges_right_to_left(char *heights, char edge1,
-		char edge2)
+int	ft_handle_rows_filled(char board[4][4], char *edges, int board_dimension)
 {
-	char	max_height_so_far2;
-	int		visible_count2;
-	int		i2;
-
-	max_height_so_far2 = '0';
-	visible_count2 = 0;
-	i2 = 3;
-	while (i2 >= 0)
+	if (ft_is_board_correct(board, edges, board_dimension))
 	{
-		if (heights[i2] > max_height_so_far2)
-		{
-			max_height_so_far2 = heights[i2];
-			visible_count2++;
-		}
-		i2--;
+		ft_print_board(board);
+		return (1);
 	}
-	return (visible_count2 == edge2 - '0');
+	return (0);
 }
 
-int	ft_check_if_vals_satisfy_edges(char *heights, char edge1, char edge2)
-{
-	return (check_if_heights_1_through_n(heights, 4)
-		&& ft_check_if_vals_satisfy_edges_left_to_right(heights, edge1, edge2)
-		&& ft_check_if_vals_satisfy_edges_right_to_left(heights, edge1, edge2));
-}
-
-int	ft_is_board_correct(char board[4][4], char *edges, int board_dimension)
-{
-	int	row;
-	int	column;
-
-	row = 0;
-	while (row < board_dimension)
-	{
-		if (!ft_check_if_vals_satisfy_edges(ft_get_row_from_board(board, row),
-				edges[8 + row], edges[12 + row]))
-		{
-			printf("row: %d\n", row);
-			return (0);
-		}
-		row++;
-	}
-	column = 0;
-	while (column < board_dimension)
-	{
-		if (!ft_check_if_vals_satisfy_edges(ft_get_column_from_board(board,
-					column), edges[0 + column], edges[4 + column]))
-		{
-			return (0);
-		}
-		column++;
-	}
-	return (1);
-}
-
+// edge1 = edges[8 + rows_filled];
+// edge2 = edges[12 + rows_filled];
+// num_rows_for_edges = ft_strstrlen(rows);
 // returns 1 if a solution was found, 0 if not
 int	ft_solve(char board[4][4], char *edges, int rows_filled,
 		int board_dimension)
 {
 	int		rtn;
-	char	edge1;
-	char	edge2;
 	char	**rows;
-	int		num_rows_for_edges;
 	int		i;
 	int		j;
 
 	if (rows_filled == 4)
+		return (ft_handle_rows_filled(board, edges, board_dimension));
+	rows = get_possible_rows_from_edges(edges[8 + rows_filled], 
+			edges[12 + rows_filled]);
+	ft_print_debugger(board, edges, rows, rows_filled);
+	i = 0;
+	while (i < ft_strstrlen(rows))
 	{
-		if (ft_is_board_correct(board, edges, board_dimension))
+		j = 0;
+		while (j < 4)
 		{
-			ft_print_board(board);
-			return (1);
+			board[rows_filled][j] = rows[i][j];
+			j++;
 		}
-		else
-			return (0);
-	}
-	else
-	{
-		edge1 = edges[8 + rows_filled];
-		edge2 = edges[12 + rows_filled];
-		rows = get_possible_rows_from_edges(edge1, edge2);
-		num_rows_for_edges = ft_strstrlen(rows);
-		i = 0;
-		ft_print_board(board);
-		printf("possible row: %s\n", rows[0]);
-		printf("edge1: %c edge2: %c num_rows: %i\n", edge1, edge2,
-			num_rows_for_edges);
-		while (i < num_rows_for_edges)
-		{
-			j = 0;
-			while (j < 4)
-			{
-				board[rows_filled][j] = rows[i][j];
-				j++;
-			}
-			rtn = ft_solve(board, edges, rows_filled + 1, board_dimension);
-			if (rtn)
-				return (rtn);
-			i++;
-		}
+		rtn = ft_solve(board, edges, rows_filled + 1, board_dimension);
+		if (rtn)
+			return (rtn);
+		i++;
 	}
 	return (0);
 }

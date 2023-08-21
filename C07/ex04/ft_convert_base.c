1629c15fb122886d22cbed04ca15bd8a684d4c7f
Rocky Kamen-Rubio <rokamen-@cbr15s3.42barcelona.com>
2023-08-21 17:40:41 +0200

    maybe working and normed C07ex04

---
+++ C07/ex04/ft_convert_base.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 15:01:40 by rokamen-          #+#    #+#             */
+/*   Updated: 2023/08/21 17:37:40 by rokamen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const static int	g_int_min = -2147483648;
+
+int					ft_isspace(char c);
+int					ft_char_to_val(char c, char *base);
+int					ft_check_base(char *base);
+long				parse_vals(char *str, int istr, char *base, int base_len);
+int					ft_atoi_base(char *str, char *base);
+
+int	ft_nbr_to_base_helper(int nbr, char *base, int base_len, char *rtn)
+{
+	int	div;
+	int	mod;
+
+	if (nbr == 0)
+	{
+		*rtn = '\0';
+		return (0);
+	}
+	div = nbr / base_len;
+	mod = nbr % base_len;
+	*rtn = base[mod];
+	return (1 + ft_nbr_to_base_helper(div, base, base_len, rtn + 1));
+}
+
+int	ft_handle_nonpositive(int nbr, char *base, int base_len, char *rtn)
+{
+	int	div;
+	int	mod;
+
+	if (nbr == 0)
+	{
+		*rtn = '0';
+		return (1);
+	}
+	else if (nbr == g_int_min)
+	{
+		div = (-1) * (g_int_min / base_len);
+		mod = (-1) * (g_int_min % base_len);
+		*rtn = '-';
+		*(rtn + 1) = base[mod];
+		return (2 + ft_nbr_to_base_helper(div, base, base_len, rtn + 2));
+	}
+	else
+	{
+		*rtn = '-';
+		nbr *= -1;
+		return (1 + ft_nbr_to_base_helper(nbr, base, base_len, rtn + 1));
+	}
+}
+
+void	ft_rev_char_tab(char *tab, int size)
+{
+	int		i;
+	char	temp;
+	char	*a;
+	char	*b;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		a = tab + i;
+		b = tab + size - i - 1;
+		if (a == b)
+			continue ;
+		temp = *a;
+		*a = *b;
+		*b = temp;
+		i++;
+	}
+}
+
+char	*ft_nbr_to_base(int nbr, char *base)
+{
+	int		base_len;
+	int		i;
+	char	*rtn;
+	int		l;
+
+	rtn = malloc(1 * 32);
+	base_len = ft_check_base(base);
+	if (base_len <= 1)
+		return (NULL);
+	if (nbr <= 0)
+		l = ft_handle_nonpositive(nbr, base, base_len, rtn);
+	else
+		l = ft_nbr_to_base_helper(nbr, base, base_len, rtn);
+	ft_rev_char_tab(rtn, l);
+	return (rtn);
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int	num;
+
+	num = ft_atoi_base(nbr, base_from);
+	return (ft_nbr_to_base(num, base_to));
+}
---
+++ C07/ex04/ft_convert_base2.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 16:19:24 by rokamen-          #+#    #+#             */
+/*   Updated: 2023/08/21 17:29:58 by rokamen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isspace(char c)
+{
+	return (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t'
+		|| c == '\v');
+}
+
+// returns -1 if value is not found in base
+int	ft_char_to_val(char c, char *base)
+{
+	int	i;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == c)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
+
+// process base: get its length, check for forbidden chars (+- ),
+//	check for repeated chars
+
+int	ft_check_base(char *base)
+{
+	int	base_index;
+	int	ibase;
+
+	base_index = 0;
+	while (*(base + base_index) != '\0')
+	{
+		ibase = 0;
+		if ((base[base_index] == '+' || base[base_index] == '-')
+			|| base[base_index] <= ' ' || base[base_index] >= 127)
+			return (-1);
+		while (ibase < base_index - 1)
+		{
+			if (base[base_index] == base[ibase])
+			{
+				return (-1);
+			}
+			ibase++;
+		}
+		base_index++;
+	}
+	return (base_index);
+}
+
+long	parse_vals(char *str, int istr, char *base, int base_len)
+{
+	long	so_far;
+
+	so_far = 0;
+	while (ft_char_to_val(str[istr], base) != -1 && str[istr] != '\0')
+	{
+		so_far *= (long)base_len;
+		so_far += (long)ft_char_to_val(str[istr], base);
+		istr++;
+	}
+	return (so_far);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int		istr;
+	int		sign;
+	long	so_far;
+	int		base_len;
+
+	istr = 0;
+	sign = 1;
+	while (ft_isspace(str[istr]))
+	{
+		istr++;
+	}
+	while (str[istr] == '-' || str[istr] == '+')
+	{
+		if (str[istr] == '-')
+			sign *= -1;
+		istr++;
+	}
+	base_len = ft_check_base(base);
+	if (base_len <= 1)
+		return (0);
+	so_far = parse_vals(str, istr, base, base_len);
+	so_far *= (long)sign;
+	return (so_far);
+}

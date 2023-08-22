cca9c21aca55b68ec98ea672dfc7f7f1841f93e5
Rocky Kamen-Rubio <rokamen-@cbr3s4.42barcelona.com>
2023-08-22 12:52:47 +0200

    more ex04 tests. Seems to compile and pass norm

--- C07/tests-C07/ex04C07_test.c
+++ C07/tests-C07/ex04C07_test.c
@@ -1,1 +1,33 @@
+//#include "../ex04/ft_convert_base.c"
+//#include "utils.c"
 
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to);
+void	ft_rev_char_tab(char *tab, int size);
+char	*ft_nbr_to_base(int nbr, char *base);
+
+int main(void){
+	char base10[] = "0123456789";
+	char base_hex[] = "0123456789abcdef";
+	char base_binary[] = "01";
+	//test ft_nbr_to_base
+    printf("%s\n", ft_nbr_to_base(123456789, "0123456789abcdef"));
+
+	//test reverse
+    char str[] = "abcde";
+    printf("%s\n", str);
+    ft_rev_char_tab(str, 5);
+    printf("%s\n", str);
+	
+	char *str2 = ft_convert_base("12345", base10, base_hex);
+	printf("%s\n", str2);
+	assert(strcmp(str2, "3039") == 0);
+
+	char *str3 = ft_convert_base("12345", base_hex, base_binary);
+	printf("%s\n", str3);
+	assert(strcmp(str3, "10010001101000101") == 0);
+}

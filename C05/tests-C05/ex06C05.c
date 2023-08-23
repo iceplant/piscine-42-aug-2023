7c3c8ef824253b1f56df5b9cf608c54ccc78f01d
Rocky Kamen-Rubio <rokamen-@cbr13s4.42barcelona.com>
2023-08-23 19:11:25 +0200

    adds C05 test

---
+++ C05/tests-C05/ex06C05.c
@@ -0,0 +1,20 @@
+#include "../ex06/ft_is_prime.c"
+#include <stdio.h>
+#include <assert.h>
+
+int main(void) {
+	assert(ft_is_prime(0) == 0);
+	assert(ft_is_prime(1) == 0);
+	assert(ft_is_prime(2) == 1);
+	assert(ft_is_prime(3) == 1);
+	assert(ft_is_prime(42) == 0);
+	assert(ft_is_prime(7919) == 1);
+	assert(ft_is_prime(-1) == 0);
+	assert(ft_is_prime(-2) == 0);
+	assert(ft_is_prime(-3) == 0);
+	assert(ft_is_prime(-42) == 0);
+	assert(ft_is_prime(-7919) == 0);	
+	assert(ft_is_prime(2147483629) == 1); 
+	assert(ft_is_prime(2147483647) == 0);
+	assert(ft_is_prime(-2147483648) == 0);
+}

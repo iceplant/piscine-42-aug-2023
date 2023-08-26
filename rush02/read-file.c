76a8cb5c490cde22a4fb12d4271ad0bbd62343dc
Rocky Kamen-Rubio <rokamen-@cbr10s2.42barcelona.com>
2023-08-26 17:20:39 +0200

    working up to 2 digits I think

--- rush02/read-file.c
+++ rush02/read-file.c
@@ -6,7 +6,7 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 13:55:34 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/26 15:28:53 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/26 17:20:24 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 int ft_is_this_line_a_match(char *number, char *line)
 {
@@ -70,16 +71,67 @@ void ft_find_and_put_value_from_dict(char *nb, char *dict_str)
 	{
 		if (i == 0 || dict_str[i - 1] == '\n')
 		{
-			printf("reached line: %s \n\n and %s \n\n\n\n\n", nb, dict_str + i);
+			//printf("reached line: %s \n\n and %s \n\n\n\n\n", nb, dict_str + i);
 			if (ft_is_this_line_a_match(nb, dict_str + i))
 			{
-				printf("Match found!\n");		
+				//printf("Match found!\n");		
 				ft_put_word_from_line(dict_str + i);
 				return ;		
 			}
 		}
 	i++;
 	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+void	ft_strncpy(char *dest, char *src, int n)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
+
+char	*ft_strndup(char *src, int n)
+{
+	char	*rtn;
+
+	rtn = malloc(n + 1);
+	ft_strncpy(rtn, src, n);
+	return (rtn);
+}
+
+char	*ft_strndup_with_zeros(char *src, int n, int num_zeros)
+{
+	char	*rtn;
+	int i;
+
+	rtn = malloc(n + num_zeros + 1);
+	ft_strncpy(rtn, src, n);
+	i = 0;
+	while (i < num_zeros)
+	{
+		rtn[n + i] = '0';
+		i++;
+	}
+	rtn[n + i] = '\0';
+	return (rtn);
 }
 
 
@@ -95,11 +147,12 @@ void ft_find_and_put_value_from_dict(char *nb, char *dict_str)
 //
 
 
-/*
-void  ft_put_words_from_value(char *str)
+
+void  ft_put_words_from_value(char *str, char* dict_str)
 {
 	int i;
 	int digit;
+	char *to_find;
 
 	i = 0;
 	
@@ -108,15 +161,41 @@ void  ft_put_words_from_value(char *str)
 	{
 		digit++;
 	}
+	//printf("digits: %d\n", digit);
+	
 	while(str[i] >= '0' && str[i] <= '9')
 	{
+		//digits are indexing from 1!
 		if (digit == 1)
+		{
+			//printf("hit digit == 1\n");
 			// look up str[i]
+			
+			to_find = ft_strndup(str + i, 1);
+			//printf("to_find: %s\n", to_find);
+			ft_find_and_put_value_from_dict(to_find, dict_str);
+		}
 		if (digit == 2)
-			//look up str[i]
-			ft_find_value_in_dict(str[i]*10);
-			// add "-" with no space between this and next value (unless the next value is zero)
-		if (digit == 3)
+		{
+			if (str[i] == '1' || str[i + 1] == '0')
+			{
+				to_find = ft_strndup(str + i, 2);
+				//printf("to find: %s\n", to_find);
+				ft_find_and_put_value_from_dict(to_find, dict_str);
+				return ;
+				//miramos adelante para los unideles
+				//hacemos una incrementacion extra
+			} else { 
+				to_find = ft_strndup_with_zeros(str + i, 1, 1);
+				//printf("to find: %s\n", to_find);
+				
+				ft_find_and_put_value_from_dict(to_find, dict_str);
+				if (str[i + 1] != '0')
+					write(1, "-", 1);
+			}
+		}
+								
+		/*if (digit == 3)
 			// look up str[i]
 			// add word for "hundred" that is in the dictionary
 			// add " and " if either of the next values are not zero
@@ -137,14 +216,12 @@ void  ft_put_words_from_value(char *str)
 		if (digit == 10)
 			// look up str[i]
 			// look up 10^9 = 1 billion
-		i--;
-		digit++;
+		*/
+		i++;
+		digit--;
 	}
 
 }
-
-
-*/
 
 
 // Driver code
@@ -152,7 +229,7 @@ int main()
 {
     int fd;
 	int sz;
-    char* dict_str = (char*)calloc(100, sizeof(char));
+    char* dict_str = (char*)malloc(1000 * sizeof(char));
  
     // Opening file in reading mode
     fd = open("numbers.dict", O_RDONLY);
@@ -166,17 +243,17 @@ int main()
     //       " %d bytes were read.\n",
     //       fd, sz);
     dict_str[sz] = '\0';
-    //printf("Those bytes are as follows: % s\n", dict_str);
-	//
-	
+
+	/*
+    printf("Those bytes are as follows: % s\n", dict_str);
 	char *number = "5";
 	ft_find_and_put_value_from_dict(number, dict_str);
-	
-	
-
 	char *test_line = "13: thirteen";
-//	int output = ft_is_this_line_a_match(number, test_line);
-//	printf("result: %d\n", output);
+	int output = ft_is_this_line_a_match(number, test_line);
+	printf("result: %d\n", output); */
+
+
+	ft_put_words_from_value("24", dict_str);
 
     
 	close(fd);

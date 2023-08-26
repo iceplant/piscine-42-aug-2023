e44167e5ef0d8e6789db4706041a2caf25e2a1e0
Rocky Kamen-Rubio <rjkr@berkeley.edu>
2023-08-26 22:56:44 +0200

    changes to librf I thnk

--- C09/ex00/libft_creator.sh
+++ C09/ex00/libft_creator.sh
@@ -10,19 +10,12 @@
 #                                                                              #
 # **************************************************************************** #
 
+cc -Wall -Wextra -Werror ft_putchar.c -c
+cc -Wall -Wextra -Werror ft_strcmp.c -c
+cc -Wall -Wextra -Werror ft_swap.c -c
+cc -Wall -Wextra -Werror ft_putstr.c -c
+cc -Wall -Wextra -Werror ft_strlen.c -c
+
 ar rcs libft.a ft_putchar.c ft_strcmp.c ft_swap.c ft_putstr.c ft_strlen.c
 
-#cc -Wall -Wextra -Werror ft_putchar.c -o ft_putchar.sh
-#cc -Wall -Wextra -Werror ft_strcmp.c -o ft_strcmp.sh
-#cc -Wall -Wextra -Werror ft_swap.c -o ft_swap.sh
-#cc -Wall -Wextra -Werror ft_putstr.c -o ft_putstr.sh
-#cc -Wall -Wextra -Werror ft_strlen.c -o ft_strlen.sh
 
-# # try to get this working?
-#names='ft_putchar.c ft_strcmp.c ft_swap.c ft_putstr.c ft_strlen.c'
-#for name in $names
-#do
-#	result=$(echo $name | sed "s/$.c/$.sh/g")
-#	echo $result	
-#done
-#echo All done

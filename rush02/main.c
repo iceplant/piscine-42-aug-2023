1180f10e6bb7afcf0d0b876ef4e53c7adcbb3547
Rocky Kamen-Rubio <rokamen-@cbr11s5.42barcelona.com>
2023-08-27 15:15:19 +0200

    some linting

--- rush02/main.c
+++ rush02/main.c
@@ -10,25 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "ft_atoi.c"
+#include <stdio.h>
 #include <unistd.h>
+
 //#include "read-file.c"
 
+char	*ft_get_dict_str_from_file(char *filename);
+void	ft_put_words_from_value(char *str, char *dict_str);
 
-char *ft_get_dict_str_from_file(char *filename);
-void ft_put_words_from_value(char *str, char *dict_str);
-
-
-
-int ft_is_digit(char c)
+int	ft_is_digit(char c)
 {
-	return c >= '0' && c <= '9';
+	return (c >= '0' && c <= '9');
 }
 
-int ft_is_printable(char c)
+int	ft_is_printable(char c)
 {
-	return c >= 32 && c <= 126;
+	return (c >= 32 && c <= 126);
 }
 
 int	ft_strcmp(char *s1, char *s2)
@@ -55,96 +53,98 @@ int	ft_strcmp(char *s1, char *s2)
 	return (s1[iter] - s2[iter]);
 }
 
-
 // TODO
-int ft_is_dict_str_formatted_correctly(char *dict_str)
+int	ft_is_dict_str_formatted_correctly(char *dict_str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (dict_str[i])
 	{
 		if (!ft_is_digit(dict_str[i]))
-				return (0);
-		while(ft_is_digit(dict_str[i]))
+			return (0);
+		while (ft_is_digit(dict_str[i]))
 		{
 			i++;
 		}
 		while (dict_str[i] == ' ')
-		{i++;}
+		{
+			i++;
+		}
 		if (dict_str[i] != ':')
 			return (0);
 		i++;
 		while (dict_str[i] == ' ')
-		{i++;}
+		{
+			i++;
+		}
 		if (!ft_is_printable(dict_str[i]))
 			return (0);
 		while (ft_is_printable(dict_str[i]))
-		{i++;}
+		{
+			i++;
+		}
 		while (dict_str[i] == '\n')
-		{i++;}
+		{
+			i++;
+		}
 	}
-  return (1);
+	return (1);
 }
 
-
-char *format_number(char *str)
+char	*format_number(char *str)
 {
-  int i;
-  char *rtn;
-  int neg_count;
+	int		i;
+	char	*rtn;
+	int		neg_count;
 
-  i = 0;
+	i = 0;
 	neg_count = 0;
-  while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-  {
-    i++;
-  }
-  while (str[i] == '+' || str[i] == '-')
-  {
-	  if (str[i] == '-')
-		  neg_count++;
-    i++;
-  }
-  rtn = str + i;
-  while (str[i] >= '0' && str[i] <= '9')
-  {
-    i++;
-  }
-  str[i] = '\0';
-  return (rtn);
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+	{
+		i++;
+	}
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			neg_count++;
+		i++;
+	}
+	rtn = str + i;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		i++;
+	}
+	str[i] = '\0';
+	return (rtn);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-  char *num_str;
-  //char *dict;
-  char *dict_str;
-  if (argc == 2)
-  {
-    num_str = argv[1];
-    dict_str = ft_get_dict_str_from_file("numbers.dict");
-  }
-  else if (argc == 3)
-  {
-    num_str = argv[1];
-    dict_str = argv[2];
-  }
-  else
-  {
-    write(1, "Error\n", 6);
-	return (0);
-  }
+	char	*num_str;
+	char	*dict_str;
 
-  if(!ft_is_dict_str_formatted_correctly(dict_str))
-	  write(1, "Dict Error\n", 11);
-
+	// char *dict;
+	if (argc == 2)
+	{
+		num_str = argv[1];
+		dict_str = ft_get_dict_str_from_file("numbers.dict");
+	}
+	else if (argc == 3)
+	{
+		num_str = argv[1];
+		dict_str = argv[2];
+	}
+	else
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+	if (!ft_is_dict_str_formatted_correctly(dict_str))
+		write(1, "Dict Error\n", 11);
 	num_str = format_number(num_str);
-  if (!num_str || ft_strcmp("4294967295", num_str) < 0 )
-	  write(1, "Error\n", 6);
-
-
-
+	if (!num_str || ft_strcmp("4294967295", num_str) < 0)
+		write(1, "Error\n", 6);
 	ft_put_words_from_value(num_str, dict_str);
 	write(1, "\n", 1);
 }
--- rush02/read-file.c
+++ rush02/read-file.c
@@ -6,272 +6,213 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 13:55:34 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/26 18:31:02 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/27 15:14:36 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // C program to implement
 // the above approach
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
 
-int ft_is_this_line_a_match(char *number, char *line)
+int		ft_strlen(char *str);
+void	ft_strncpy(char *dest, char *src, int n);
+char	*ft_strndup(char *src, int n);
+char	*ft_strndup_with_zeros(char *src, int n, int num_zeros);
+
+int	ft_is_this_line_a_match(char *number, char *line)
 {
-  int i;
+	int	i;
 
-  i = 0;
-  while (number[i])
-  {
-    if (number[i] != line[i])
-    {
-      return (0);
-    }
-    i++;
-  }
-  if (line[i] >= '0' && line[i] <= '9')
-  {
-    return (0);
-  }
-  return (1);
+	i = 0;
+	while (number[i])
+	{
+		if (number[i] != line[i])
+		{
+			return (0);
+		}
+		i++;
+	}
+	if (line[i] >= '0' && line[i] <= '9')
+	{
+		return (0);
+	}
+	return (1);
 }
 
-void ft_put_word_from_line(char *line)
+void	ft_put_word_from_line(char *line)
 {
-  int i;
+	int	i;
 
-  i = 0;
-  while (line[i] >= '0' && line[i] <= '9')
-  {
-    i++;
-  }
-
-  while (line[i] == ' ' || line[i] == ':')
-  {
-    i++;
-  }
-  while (line[i] != '\n' && line[i] != '\0')
-  {
-    write(1, line + i, 1);
-    i++;
-  }
+	i = 0;
+	while (line[i] >= '0' && line[i] <= '9')
+	{
+		i++;
+	}
+	while (line[i] == ' ' || line[i] == ':')
+	{
+		i++;
+	}
+	while (line[i] != '\n' && line[i] != '\0')
+	{
+		write(1, line + i, 1);
+		i++;
+	}
 }
 
-int ft_find_and_put_value_from_dict(char *nb, char *dict_str)
+int	ft_find_and_put_value_from_dict(char *nb, char *dict_str)
 {
-  int i;
+	int	i;
 
-  i = 0;
-  while (dict_str[i])
-  {
-    if (i == 0 || dict_str[i - 1] == '\n')
-    {
-      // printf("reached line: %s \n\n and %s \n\n\n\n\n", nb, dict_str + i);
-      if (ft_is_this_line_a_match(nb, dict_str + i))
-      {
-        // printf("Match found!\n");
-        ft_put_word_from_line(dict_str + i);
-        return (1);
-      }
-    }
-    i++;
-  }
-  return (0);
+	i = 0;
+	while (dict_str[i])
+	{
+		if (i == 0 || dict_str[i - 1] == '\n')
+		{
+			// printf("reached line: %s \n\n and %s \n\n", nb, dict_str + i);
+			if (ft_is_this_line_a_match(nb, dict_str + i))
+			{
+				// printf("Match found!\n");
+				ft_put_word_from_line(dict_str + i);
+				return (1);
+			}
+		}
+		i++;
+	}
+	return (0);
 }
 
-int ft_strlen(char *str)
+void	ft_put_words_from_value(char *str, char *dict_str)
 {
-  int i;
+	int		i;
+	int		digit;
+	char	*to_find;
+	int		nonzero;
 
-  i = 0;
-  while (str[i])
-  {
-    i++;
-  }
-  return (i);
-}
-
-void ft_strncpy(char *dest, char *src, int n)
-{
-  int i;
-
-  i = 0;
-  while (src[i] && i < n)
-  {
-    dest[i] = src[i];
-    i++;
-  }
-  dest[i] = '\0';
-}
-
-char *ft_strndup(char *src, int n)
-{
-  char *rtn;
-
-  rtn = malloc(n + 1);
-  ft_strncpy(rtn, src, n);
-  return (rtn);
-}
-
-char *ft_strndup_with_zeros(char *src, int n, int num_zeros)
-{
-  char *rtn;
-  int i;
-
-  rtn = malloc(n + num_zeros + 1);
-  ft_strncpy(rtn, src, n);
-  i = 0;
-  while (i < num_zeros)
-  {
-    rtn[n + i] = '0';
-    i++;
-  }
-  rtn[n + i] = '\0';
-  return (rtn);
-}
-
-/*
- * "     ++--+-+1234asbacdf" =>
- * 	- go to end of the number
- * 	- start with smallest digit
- * 	- iterate to the left, multiplying each digit by the number of iterations before looking up in table
- *
- */
-// ponemos zeros para que siempre tenga exactamente 10 characteres?
-//
-
-void ft_put_words_from_value(char *str, char *dict_str)
-{
-  int i;
-  int digit;
-  char *to_find;
-  int nonzero = 0;
-
-  i = 0;
-
-  digit = 0;
-  while (str[digit] >= '0' && str[digit] <= '9')
-  {
-    if (str[digit] != '0')
-      nonzero = 1;
-    digit++;
-  }
-  // printf("digits: %d\n", digit);
-
-  // TODO: handle zero as special case
-  if (!nonzero)
-  {
-    if (ft_find_and_put_value_from_dict("0", dict_str))
-      return;
-    else
-      write(1, "Dict Error\n", 11);
-  }
-
-  // TODO: check if entire number is present in dictionary
-
-  if (ft_find_and_put_value_from_dict(str, dict_str))
-    return;
-
-  while (str[i] >= '0' && str[i] <= '9')
-  {
-    // digits are indexing from 1!
-    if (digit % 3 == 1 && str[i] != '0')
-    {
-      to_find = ft_strndup(str + i, 1);
-      ft_find_and_put_value_from_dict(to_find, dict_str);
-    }
-    if (digit % 3 == 2 && str[i] != '0')
-    {
-      if (str[i] == '1' || str[i + 1] == '0')
-      {
-        to_find = ft_strndup(str + i, 2);
-        ft_find_and_put_value_from_dict(to_find, dict_str);
-        i += 1;
-        digit--;
-      }
-      else
-      {
-        to_find = ft_strndup_with_zeros(str + i, 1, 1);
-        // printf("to find: %s\n", to_find);
-
-        ft_find_and_put_value_from_dict(to_find, dict_str);
-        if (str[i] != '0' && str[i + 1] != '0')
-          write(1, "-", 1);
-      }
-    }
-
-    if (digit % 3 == 0 && str[i] != '0')
-    {
-      // look up str[i]
-      // add word for "hundred" that is in the dictionary
-      // add " and " if either of the next values are not zero
-      to_find = ft_strndup(str + i, 1);
-      ft_find_and_put_value_from_dict(to_find, dict_str);
-      write(1, " hundred", 8);
-      if (str[i + 1] != '0' || str[i + 2] != '0')
-        write(1, " and ", 5);
-    }
-    if (digit == 4)
-    {
-      write(1, " ", 1);
-      ft_find_and_put_value_from_dict("1000", dict_str);
-      write(1, " ", 1);
-    }
-    if (digit == 7)
-    {
-      write(1, " ", 1);
-      ft_find_and_put_value_from_dict("1000000", dict_str);
-      write(1, " ", 1);
-    }
-    if (digit == 10)
-    {
-      write(1, " ", 1);
-      ft_find_and_put_value_from_dict("1000000000", dict_str);
-      write(1, " ", 1);
-    }
-    i++;
-    digit--;
-  }
+	nonzero = 0;
+	i = 0;
+	digit = 0;
+	while (str[digit] >= '0' && str[digit] <= '9')
+	{
+		if (str[digit] != '0')
+			nonzero = 1;
+		digit++;
+	}
+	if (!nonzero)
+	{
+		if (ft_find_and_put_value_from_dict("0", dict_str))
+			return ;
+		else
+			write(1, "Dict Error\n", 11);
+	}
+	// TODO: check if entire number is present in dictionary
+	if (ft_find_and_put_value_from_dict(str, dict_str))
+		return ;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		// digits are indexing from 1!
+		if (digit % 3 == 1 && str[i] != '0')
+		{
+			to_find = ft_strndup(str + i, 1);
+			ft_find_and_put_value_from_dict(to_find, dict_str);
+		}
+		if (digit % 3 == 2 && str[i] != '0')
+		{
+			if (str[i] == '1' || str[i + 1] == '0')
+			{
+				to_find = ft_strndup(str + i, 2);
+				ft_find_and_put_value_from_dict(to_find, dict_str);
+				i += 1;
+				digit--;
+			}
+			else
+			{
+				to_find = ft_strndup_with_zeros(str + i, 1, 1);
+				// printf("to find: %s\n", to_find);
+				ft_find_and_put_value_from_dict(to_find, dict_str);
+				if (str[i] != '0' && str[i + 1] != '0')
+					write(1, "-", 1);
+			}
+		}
+		if (digit % 3 == 0 && str[i] != '0')
+		{
+			// look up str[i]
+			// add word for "hundred" that is in the dictionary
+			// add " and " if either of the next values are not zero
+			to_find = ft_strndup(str + i, 1);
+			ft_find_and_put_value_from_dict(to_find, dict_str);
+			write(1, " hundred", 8);
+			if (str[i + 1] != '0' || str[i + 2] != '0')
+				write(1, " and ", 5);
+		}
+		if (digit == 4)
+		{
+			write(1, " ", 1);
+			ft_find_and_put_value_from_dict("1000", dict_str);
+			write(1, " ", 1);
+		}
+		if (digit == 7)
+		{
+			write(1, " ", 1);
+			ft_find_and_put_value_from_dict("1000000", dict_str);
+			write(1, " ", 1);
+		}
+		if (digit == 10)
+		{
+			write(1, " ", 1);
+			ft_find_and_put_value_from_dict("1000000000", dict_str);
+			write(1, " ", 1);
+		}
+		i++;
+		digit--;
+	}
 }
 
 /*
 // debugging
-char *to_str(int i)
+char	*to_str(int i)
 {
-  char d1 = (i / 10) + '0';
-  char d2 = (i % 10) + '0';
-  char *rtn = malloc(sizeof(char) * 3);
+	char	d1;
+	char	d2;
+	char	*rtn;
+	int		fd;
+	int		sz;
+	char	*dict_str;
+	char	*dict_str;
+	int		fd;
+	int		sz;
+
+  d1 = (i / 10) + '0';
+  d2 = (i % 10) + '0';
+  rtn = malloc(sizeof(char) * 3);
   rtn[0] = d1;
   rtn[1] = d2;
-
-  return rtn;
+  return (rtn);
 } */
-
-char *ft_get_dict_str_from_file(char *filename)
+char	*ft_get_dict_str_from_file(char *filename)
 {
-  int fd;
-  int sz;
-  char *dict_str = (char *)malloc(1000 * sizeof(char));
-  fd = open(filename, O_RDONLY);
-
-  if (0 == fd)
-  {
-    printf("file can't be opened \n");
-    return NULL;
-  }
-
-  sz = read(fd, dict_str, 100000);
-  dict_str[sz] = '\0';
-  close(fd);
-  return dict_str;
+	char	*dict_str;
+	int		fd;
+	int		sz;
+	
+	dict_str = (char *)malloc(1000 * sizeof(char));
+	fd = open(filename, O_RDONLY);
+	if (0 == fd)
+	{
+		printf("file can't be opened \n");
+		return (NULL);
+	}
+	sz = read(fd, dict_str, 100000);
+	dict_str[sz] = '\0';
+	close(fd);
+	return (dict_str);
 }
 
 /*
-int main()
+int	main(void)
 {
   char *dict_str;
   dict_str = ft_get_dict_str_from_file("numbers.dict");
@@ -279,11 +220,11 @@ int main()
   char number_str[11];
   while (i <= 1362451256)
   {
-    sprintf(number_str, "%d", i);
-    ft_put_words_from_value(number_str, dict_str);
-    printf("\n");
-    i++;
+	sprintf(number_str, "%d", i);
+	ft_put_words_from_value(number_str, dict_str);
+	printf("\n");
+	i++;
   }
   free(dict_str);
-  return 0;
+  return (0);
 } */

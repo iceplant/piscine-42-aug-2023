7f0b4478fd3cb853deb7f20be900dbbe2793c9f7
Rocky Kamen-Rubio <rokamen-@cbr14s3.42barcelona.com>
2023-08-30 19:09:02 +0200

    ex05 seems to work

--- C11/ex05/doop.c
+++ C11/ex05/doop.c
@@ -6,17 +6,17 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 18:23:36 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/30 18:58:43 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/30 19:07:08 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
-int ft_atoi(char *str);
-void ft_putnbr(int nb);
+int		ft_atoi(char *str);
+void	ft_putnbr(int nb);
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
 	int	i;
 
@@ -26,38 +26,40 @@ int ft_strlen(char *str)
 	return (i);
 }
 
-int main(int argc, char **argv)
+void	handle_valid_operators_with_an_o(char op, int a, int b)
 {
-	int	a;
-	int b;
-	if (argc == 4 && ft_strlen(argv[2]) == 1)
+	if (op == '+')
+		ft_putnbr(a + b);
+	else if (op == '-')
+		ft_putnbr(a - b);
+	else if (op == '*')
+		ft_putnbr(a * b);
+	else if (op == '/')
 	{
-		a = ft_atoi(argv[1]);
-		b = ft_atoi(argv[3]);
-		if (argv[2][0] == '+')
-			ft_putnbr(a + b);
-		else if (argv[2][0] == '-')
-			ft_putnbr(a - b);
-		else if (argv[2][0] == '*')
-			ft_putnbr(a*b);
-		else if (argv[2][0] == '/')
-		{
-			if (b == 0)
-				write(1, "Stop : division by zero", 23);
-			else
-				ft_putnbr(a / b);
-		}
-		else if (argv[2][0] == '%')
-		{
-			if (b == 0)
-				write(1, "Stop : modulo by zero", 21);
-			else
-				ft_putnbr(a % b);
-		}
-		else 
-			write(1, "0", 1);
+		if (b == 0)
+			write(1, "Stop : division by zero", 23);
+		else
+			ft_putnbr(a / b);
 	}
-	else 
+	else if (op == '%')
+	{
+		if (b == 0)
+			write(1, "Stop : modulo by zero", 21);
+		else
+			ft_putnbr(a % b);
+	}
+	else
+		write(1, "0", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 4 && ft_strlen(argv[2]) == 1)
+		handle_valid_operators_with_an_o(
+			argv[2][0],
+			ft_atoi(argv[1]), 
+			ft_atoi(argv[3]));
+	else
 		write(1, "0", 1);
 	write(1, "\n", 1);
 }

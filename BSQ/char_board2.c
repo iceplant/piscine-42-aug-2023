c31f597275da44828431db60e326e36c3472075e
Rocky Kamen-Rubio <rokamen-@cbr14s3.42barcelona.com>
2023-08-30 14:13:55 +0200

    more changes

--- BSQ/char_board2.c
+++ BSQ/char_board2.c
@@ -6,11 +6,16 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 13:50:27 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/30 13:52:42 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/30 14:11:25 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// for write function
+#include <unistd.h>
+
 int		get_char_board_info_line_length(char *char_board);
+int		get_char_board_line_length(char *char_board);
+char	get_char_board_val(char *char_board, int row, int col);
 
 char	obstacle(char *char_board)
 {
@@ -42,4 +47,30 @@ char	get_char_board_num_rows(char *char_board)
 		i++;
 	}
 	return (so_far);
+}
+
+void	put_char_board(char *char_board)
+{
+	int	num_rows;
+	int	num_columns;
+	int	row;
+	int	col;
+	char to_write;
+
+	row = 0;
+	col = 0;
+	num_rows = get_char_board_num_rows(char_board);
+	num_columns = get_char_board_line_length(char_board) - 1;
+	while (row < num_rows)
+	{
+		col = 0;
+		while (col < num_columns - 1)
+		{
+			to_write = get_char_board_val(char_board, row, col);
+			write(1, &to_write,  1);
+			col++;
+		}
+		write(1, "\n", 1);
+		row++;
+	}
 }
--- BSQ/main.c
+++ BSQ/main.c
@@ -6,7 +6,7 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 17:26:36 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/30 13:14:49 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/30 14:13:45 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ char	*ft_get_dict_str_from_file(char *filename);
 int		get_char_board_line_length(char *char_board);
 char	get_char_board_val(char *char_board, int row, int col);
 void	set_char_board_val(char *char_board, int row, int col, char val);
+int		get_char_board_num_rows(char *char_board);
+void	put_char_board(char *char_board);
 
 // int_board.c
 int		*make_int_board(int num_rows, int num_columns);
@@ -32,38 +34,22 @@ void	set_int_board_val(int *int_board, int row, int col, int val);
 void	set_numbers(char *char_board, int *int_board);
 void	fill_square(char *char_board, int *int_board);
 
+void	solve_file(char *filename)
+{
+	char	*char_board;
+	int		*int_board;
+
+	char_board = ft_get_dict_str_from_file(filename);
+	int_board = make_int_board(
+			get_char_board_num_rows(char_board),
+			get_char_board_line_length(char_board) - 1);
+	set_numbers(char_board, int_board);
+	fill_square(char_board, int_board);
+	put_char_board(char_board);
+}
+
 int	main(void)
 {
-	char	*rtn;
-	int		*int_board;
-	int		num_lines;
-	char	empty;
-	char	obstacle;
-	char	full;
-
-	rtn = ft_get_dict_str_from_file("internet_example_file");
-	printf("%d\n", get_char_board_line_length(rtn));
-	printf("%d\n", get_char_board_line_length(rtn));
-	printf("%d\n", get_char_board_line_length(rtn));
-	printf("%d\n", get_char_board_line_length(rtn));
-	set_char_board_val(rtn, 5, 5, 'X');
-	printf("%c\n", get_char_board_val(rtn, 5, 5));
-	int_board = make_int_board(9, get_char_board_line_length(rtn) - 1);
-	set_int_board_val(int_board, 5, 5, 42);
-	printf("%d\n", get_int_board_val(int_board, 5, 5));
-	printf("%c\n", rtn[0]);
-	set_numbers(rtn, int_board);
-	print_int_board(int_board);
-	fill_square(rtn, int_board);
-	printf("%s\n", rtn);
-	/*
-	num_lines = rtn[0] - '0';
-	empty = rtn[1];
-	obstacle = rtn[2];
-	full = rtn[3];
-	printf("%s\n", rtn);
-	ft_backtrack(rtn);
-	printf("%s\n", rtn);
-	*/
+	solve_file("internet_example_file");
 	return (0);
 }

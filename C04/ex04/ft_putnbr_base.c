9433f87027e44a73204e94983d6ba1541c4a66c3
Rocky Kamen-Rubio <rokamen-@cbr14s3.42barcelona.com>
2023-08-31 14:20:55 +0200

    some fixes to C04ex04

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -6,13 +6,13 @@
 /*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 20:49:15 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/08/17 15:49:57 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/08/31 14:20:33 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-const static int	g_int_min = -2147483648;
+// const static int	g_int_min = -2147483648;
 
 void	ft_putnbr_base_helper(int nbr, char *base, int base_len)
 {
@@ -31,15 +31,17 @@ void	ft_handle_nonpositive(int nbr, char *base, int base_len)
 {
 	int	div;
 	int	mod;
+	int	int_min;
 
+	int_min = -2147483648;
 	if (nbr == 0)
 	{
 		write(1, &base[0], 1);
 	}
-	else if (nbr == g_int_min)
+	else if (nbr == int_min)
 	{
-		div = (-1) * (g_int_min / base_len);
-		mod = (-1) * (g_int_min % base_len);
+		div = (-1) * (int_min / base_len);
+		mod = (-1) * (int_min % base_len);
 		write(1, "-", 1);
 		ft_putnbr_base_helper(div, base, base_len);
 		write(1, &base[mod], 1);
@@ -52,7 +54,8 @@ void	ft_handle_nonpositive(int nbr, char *base, int base_len)
 	}
 }
 
-void	ft_putnbr_base(int nbr, char *base)
+// returns base length if works correctly
+int	ft_check_base(char *base)
 {
 	int	base_len;
 	int	i;
@@ -61,20 +64,30 @@ void	ft_putnbr_base(int nbr, char *base)
 	while (*(base + base_len) != '\0')
 	{
 		i = 0;
-		if (base[base_len] == '+' || base[base_len] == '-')
-			return ;
+		if (base[base_len] == '+' || base[base_len] == '-'
+			|| base[base_len] <= 32 || base[base_len] == 127)
+			return (0);
 		while (i < base_len)
 		{
 			if (base[base_len] == base[i])
-				return ;
+				return (0);
 			i++;
 		}
 		base_len++;
 	}
+	return (base_len);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int	base_len;
+
+	base_len = ft_check_base(base);
 	if (base_len <= 1)
 		return ;
 	if (nbr <= 0)
 		ft_handle_nonpositive(nbr, base, base_len);
 	else
 		ft_putnbr_base_helper(nbr, base, base_len);
+	write(1, "\n", 1);
 }
--- C04/tests-C04/ex04C04_test.c
+++ C04/tests-C04/ex04C04_test.c
@@ -1,7 +1,7 @@
 #include "../ex04/ft_putnbr_base.c"
 #include <stdio.h>
 
-//const static int g_int_min = -2147483648;
+const static int g_int_min = -2147483648;
 
 int main(void) {
 
@@ -11,33 +11,36 @@ int main(void) {
 	ft_putnbr_base(10, "abc");
 	
 	printf("\n999 in base '01' should be '1111100111' \n");
-	ft_putnbr_base(999, "01");
+	ft_putnbr_base(999, "01"); 
 
 	printf("\nShould output nothing: \n");
 	ft_putnbr_base(10, "");
+	//printf("1\n");
 	ft_putnbr_base(10, "0");
+	//printf("2\n");
 	ft_putnbr_base(10, "+0");
+	printf("Finished block that should output nothing\n");
 
 	
-	printf("more tests: ");
+	//printf("more tests: ");
 	ft_putnbr_base(-12, "01");
-	printf("\n");
+	//printf("\n");
 	ft_putnbr_base(40, "poneyvif");
-	printf("\n");
+	//printf("\n");
 	ft_putnbr_base(894868, "0123456789");
-	printf("\n");
+	//printf("\n");
 	ft_putnbr_base(53, "0123456789abcdef");
 
-	printf("\n");
+	//printf("\n");
 	ft_putnbr_base(g_int_min, "01");
-	printf("\n");
+	//printf("\n");
 	printf("g min: %d\n", g_int_min);
 	ft_putnbr_base(g_int_min, "0123456789");
-	printf("\n");
+	//printf("\n");
 	ft_putnbr_base(g_int_min, "0123456789abcdef");
-	printf("\n");
+	//printf("\n");
 	ft_putnbr_base(-56789, "0123456789"); 
 
-	ft_putnbr_base(g_int_min, "0123456789");
+	ft_putnbr_base(g_int_min, "0123456789"); 
 	
 }

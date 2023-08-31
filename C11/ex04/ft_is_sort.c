85ecd2d40e75197010c2a2ce0d424781ab16f821
Rocky Kamen-Rubio <rokamen-@cbr14s3.42barcelona.com>
2023-08-31 12:17:39 +0200

    lints C11ex04

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -10,35 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_is_sort_descending(int *tab, int length, int (*f)(int, int))
+int	ft_is_sort_descending(int *tab, int length, int (*f)(int, int))
 {
-  int i;
+	int	i;
 
-  i = 0;
-  while (i < length - 1)
-  {
-    if (f(tab[i], tab[i + 1]) < 0)
-      return (0);
-    i++;
-  }
-  return (1);
+	i = 0;
+	while (i < length - 1)
+	{
+		if (f(tab[i], tab[i + 1]) < 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int ft_is_sort_ascending(int *tab, int length, int (*f)(int, int))
+int	ft_is_sort_ascending(int *tab, int length, int (*f)(int, int))
 {
-  int i;
+	int	i;
 
-  i = 0;
-  while (i < length - 1)
-  {
-    if (f(tab[i], tab[i + 1]) > 0)
-      return (0);
-    i++;
-  }
-  return (1);
+	i = 0;
+	while (i < length - 1)
+	{
+		if (f(tab[i], tab[i + 1]) > 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int ft_is_sort(int *tab, int length, int (*f)(int, int))
+int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
-  return (ft_is_sort_ascending(tab, length, f) || ft_is_sort_descending(tab, length, f));
+	return (ft_is_sort_ascending(tab, length, f) || ft_is_sort_descending(tab,
+			length, f));
 }
